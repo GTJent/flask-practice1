855790efb913250ca8d5f3748230e26ff4177ed0
김준혁 <kakatunjent@gmail.com>
2026-09-22 16:46:48 +0900

    profile페이지 추가

--- HW1/app.py
+++ HW1/app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def main_template_render():
     return render_template("main.html", myname="김준혁", mynum ="22011725")
+
+@app.route("/profile")
+def profile_template_render():
+    hobbies = ["게임","영화","유튜브 시청"]
+    return render_template("profile.html",myhobbies=hobbies)
